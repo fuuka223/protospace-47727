5ea9f09f82017e922139e883adf6e3f1e11971e0
fuuka223 <putarous@gmail.com>
2026-03-27 16:57:14 +0900

    投稿内容が保存機能の設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,11 +5,16 @@ class PrototypesController < ApplicationController
   end
 
   def new
-    
+    @prototype = Prototype.new
   end
 
   def create
-
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
