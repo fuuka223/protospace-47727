aa676dbfb538ec0c7e56e88ee7f126d0bd279f32
fuuka223 <putarous@gmail.com>
2026-03-27 15:45:28 +0900

    コントローラーとルーティングの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,6 +4,14 @@ class PrototypesController < ApplicationController
     @prototypes = Prototype.all
   end
 
+  def new
+    
+  end
+
+  def create
+
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
