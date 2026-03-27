6d546a893a3e77618fc2b869111db0557e1d6b23
fuuka223 <putarous@gmail.com>
2026-03-27 15:10:49 +0900

    Active Storageの導入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,12 @@
 class PrototypesController < ApplicationController
 
   def index
-  
+    @prototypes = Prototype.all
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
 end
