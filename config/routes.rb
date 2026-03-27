ced5e69c1c6966b76648469d13d76276b9baca53
fuuka223 <putarous@gmail.com>
2026-03-27 22:24:27 +0900

    投稿内容編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   
   root to: "prototypes#index"
 
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update]
 
 end
