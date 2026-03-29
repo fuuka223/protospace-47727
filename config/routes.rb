581fc46436d768cd6be0d59b339de7f0ae220252
fuuka223 <putarous@gmail.com>
2026-03-29 13:42:07 +0900

    ユーザー詳細機能の実装　ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/show'
   get 'comments/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -15,5 +16,7 @@ Rails.application.routes.draw do
   resources :prototypes do
     resources :comments, only: :create
   end
+
+  resources :user, only: :show
 
 end
